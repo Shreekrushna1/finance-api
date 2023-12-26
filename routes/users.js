23232e41bad165b9fdcd34c03014a177f9f1f8f1
Shreekrushna1 <56877910+Shreekrushna1@users.noreply.github.com>
2023-12-26 21:40:48 +0530

    Add files via upload

--- routes/users.js
+++ routes/users.js
@@ -1,12 +1,20 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb+srv://krushnaproducts:Krushna11@cluster0.uvurvqw.mongodb.net/?retryWrites=true&w=majority');
+mongoose.connect('mongodb+srv://krushnaproducts:Krushna11@cluster0.uvurvqw.mongodb.net/yourDatabaseName?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true });
+
+const db = mongoose.connection;
+
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+db.once('open', () => {
+  console.log('Connected to MongoDB successfully');
+});
 
 const userSchema = mongoose.Schema({
   firstName: String,
   lastName: String,
   email: String,
   password: String,
-  details:Array
+  details: Array
 });
-module.exports = mongoose.model('users',userSchema);
+
+module.exports = mongoose.model('users', userSchema);
