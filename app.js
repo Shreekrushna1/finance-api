94f2401f3b052fc12957f47887fde8382e962492
Shreekrushna1 <56877910+Shreekrushna1@users.noreply.github.com>
2023-12-29 17:55:43 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var addCardRouter = require('./routes/add-new-card');
+var transaction = require('./routes/transaction');
 var cors = require('cors');
 var app = express();
 
@@ -21,6 +23,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(cors());
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/addNewCard', addCardRouter);
+app.use('/transaction', transaction);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
